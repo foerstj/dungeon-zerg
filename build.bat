999d6948e2e4ae5a173bfc03eca3cfaf01669a2a
Johannes Förstner <johannes@famfoerstner.de>
2024-12-27 16:28:46 +0100

    Repo scripts

---
+++ build.bat
@@ -0,0 +1,21 @@
+:: name of mod, case-sensitive
+set mod_cs=Dungeon Zerg
+
+:: path of Bits dir
+set bits=%~dp0.
+:: path of DS installation
+set ds=%DungeonSiege%
+:: path of TankCreator
+set tc=%TankCreator%
+
+set copyright=CC-BY-SA 2024
+set author=Johannes Förstner
+
+:: Compile main resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Cleanup
+rmdir /S /Q "%tmp%\Bits"
