c094ab067cf084090f706077d0d59a09f84ff125
Johannes Förstner <johannes@famfoerstner.de>
2025-02-11 20:05:00 +0100

    Different approach with vanilla vs. loa templates, necessary anyway to overwrite _core - still doesn't work for LoA WTF

--- build.bat
+++ build.bat
@@ -11,30 +11,22 @@ set tc=%TankCreator%
 set copyright=CC-BY-SA 2025
 set author=Johannes Förstner
 
-:: Compile main resource file
+:: Compile main resource file - Vanilla
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\art\bitmaps" "%tmp%\Bits\art\bitmaps" /S *-loa.raw /xf *.psd /xf *.bat /xf *.exe /xf *.lqd20
-setlocal EnableDelayedExpansion
-pushd "%tmp%\Bits\art\bitmaps\characters\evil_d_monsters"
-for %%F in (*-loa.raw) do (
-  set "name=%%F"
-  ren "!name!" "!name:-loa=!"
-)
-popd
-endlocal
-
-robocopy "%bits%\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
-robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E
-"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
-if %errorlevel% neq 0 pause
-
-:: Compile main resource file - vanilla
-rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\art" "%tmp%\Bits\art" /S /xf *-loa.raw /xf *.psd /xf *.bat /xf *.exe /xf *.lqd20
+robocopy "%bits%\art" "%tmp%\Bits\art" /S /xf *.psd /xf *.bat /xf *.exe /xf *.lqd20
 robocopy "%bits%\sound" "%tmp%\Bits\sound" /E
 robocopy "%bits%\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
 robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E
+robocopy "%bits%\world\contentdb\templates-vanilla" "%tmp%\Bits\world\contentdb\templates" /E
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Compile main resource file - LoA
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
+robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E
+robocopy "%bits%\world\contentdb\templates-loa" "%tmp%\Bits\world\contentdb\templates" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
