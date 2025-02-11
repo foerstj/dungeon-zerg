9ec972d72230b63bffc7e9db17945b6cd82ae7f3
Johannes Förstner <johannes@famfoerstner.de>
2025-02-11 18:37:18 +0100

    Add Chitterskrag sound resources

--- build.bat
+++ build.bat
@@ -14,7 +14,6 @@ set author=Johannes Förstner
 :: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\art\bitmaps" "%tmp%\Bits\art\bitmaps" /S *-loa.raw /xf *.psd /xf *.bat /xf *.exe /xf *.lqd20
-
 setlocal EnableDelayedExpansion
 pushd "%tmp%\Bits\art\bitmaps\characters\evil_d_monsters"
 for %%F in (*-loa.raw) do (
@@ -32,6 +31,7 @@ if %errorlevel% neq 0 pause
 :: Compile main resource file - vanilla
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\art" "%tmp%\Bits\art" /S /xf *-loa.raw /xf *.psd /xf *.bat /xf *.exe /xf *.lqd20
+robocopy "%bits%\sound" "%tmp%\Bits\sound" /E
 robocopy "%bits%\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
 robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
